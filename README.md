10059abf7a281942c27be89b61b3d67a250737eb
NathanielWatife <awosanminathaniel0@gmail.com>
2024-05-02 02:44:31 +0100

    Created a django project. Added models and Urls

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# portfolio-project
-Building my alx portfolio project 
+# HerData
+### HerData is a data management system for the Herbarium of the University of Helsinki. 
+    -It is a web application that allows the management of the herbarium's collection, including the management of specimens, loans, and users. 
+    - The system is built using the Django web framework and is designed to be used by the staff of the herbarium.
+    
---
+++ livestock/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+    
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
+
+"""_summary_
+    creating urls for the livestock app
+    login: The login page
+    logout: The logout page
+    password_change: The password change page
+    password_change_done: The password change done page
+    password_reset: The password reset page
+    password_reset_done: The password reset done page
+    reset: The password reset confirm page
+    reset_done: The password reset complete page
+"""
